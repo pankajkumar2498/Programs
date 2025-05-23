9537459091fa653c34c10dfadbd5f5f4d8a443dd
Maurya <maurya2498@gmail.com>
2025-05-24 00:47:55 +0530

    Updated Prime_Num by Seive method

--- IsPrime.java
+++ IsPrime.java
@@ -1,18 +1,35 @@
 class IsPrime {
 
-    // static boolean isPrimeNum(int n) {
-    //     if (n < 2) {
-    //         return false;
-    //     }
+      // Brute Force Approach --> O(n)
+    static boolean isPrime1(int num){
 
-    //     int sqrt = (int) Math.sqrt(n);
-    //     for (int i = 2; i * i <= sqrt; i++) {
-    //         if (n % i == 0) {
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
+        if( num < 2 ){
+            return false;
+        }
+
+        for( int i=2; i <= num; i++){
+            if( num%i == 0 ){
+                return false;
+            }
+        }
+        return true;
+    }   
+    
+    // Optimal Approach --> O(sqrt(n))
+    static boolean isPrime2(int num){
+
+        if( num < 2 ){
+            return false;
+        }
+
+        //     int sqrt = (int) Math.sqrt(num);
+        for( int i=2; i*i <= num; i++){
+            if( num%i == 0 ){
+                return false;
+            }
+        }
+        return true;
+    }   
 
     // getting how many prime numbers are there below any number
     static void seivePrime(int n, boolean[] primes) {
@@ -33,13 +50,18 @@ class IsPrime {
     }
 
     public static void main(String[] args) {
-        // int n = 25;
-        // for (int i = 0; i <= n; i++) {
-        // System.out.println(i + " " + isPrimeNum(i));
-        // }
+       
+        int num = 40;
+        // boolean ans = isPrime2(num);
 
-        int n = 40;
-        boolean[] primes = new boolean[n + 1];
-        IsPrime.seivePrime(n, primes);
+        // if(ans){
+        //     System.out.println("Prime");
+        // }else{
+        //     System.out.println("Not Prime");
+        // }
+       
+        boolean[] primes = new boolean[num + 1];
+        IsPrime.seivePrime(num, primes);
+        
     }
 }
