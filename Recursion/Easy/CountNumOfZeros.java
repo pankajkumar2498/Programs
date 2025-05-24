d8dee1483a8e101d8295e4ecbbdcfe6126f0f06c
Maurya <maurya2498@gmail.com>
2025-05-25 00:23:42 +0530

    Count No. of Zeroes by Recursion

--- Recursion/Easy/CountNumOfZeros.java
+++ Recursion/Easy/CountNumOfZeros.java
@@ -1,15 +1,32 @@
 package Recursion.Easy;
 
 public class CountNumOfZeros {
-    
-    static int countZeroes(int num){
+
+    // Brute Force Approach --> O(n)
+    static int countZeroes(int num) {
         int count = 0;
 
-        while(num != 0){
-            if( num%10 == 0){
+        while (num != 0) {
+            if (num % 10 == 0) {
                 count++;
             }
-            num= num/10;
+            num = num / 10;
+        }
+        return count;
+    }
+
+    // Recursive Approach --> O(n)
+    static int countZeroByRecursion(int num, int count) {
+        if (num == 0) {
+            return count;
+        }
+
+        while (num != 0) {
+            if (num % 10 == 0) {
+                return countZeroByRecursion(num / 10, count + 1);
+            } else {
+                return countZeroByRecursion(num / 10, count);
+            }
         }
         return count;
     }
@@ -17,13 +34,22 @@ public class CountNumOfZeros {
     public static void main(String[] args) {
         System.out.println();
 
-        int num = 900;
-        int ans = countZeroes(num);
-        
-        if(ans != 0){
-            System.out.println("Total Number of Zeroes Present in "+ num + " are: "+ ans);
-        }else{
-            System.out.println("No Zero present inside "+ num);
+        int num = 901000, count = 0;
+        int ans1 = countZeroes(num);
+
+        int ans2 = countZeroByRecursion(num, count);
+
+        if (ans1 != 0) {
+        System.out.println("Total Number of Zeroes Present in " + num + " are: " +
+        ans1);
+        } else {
+        System.out.println("No Zero present inside " + num);
+        }
+
+        if (ans2 != 0) {
+            System.out.println("Total Number of Zeroes Present in " + num + " are: " + ans2);
+        } else {
+            System.out.println("No Zero present inside " + num);
         }
     }
 }
