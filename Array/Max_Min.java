429f80c3c16229e9110bbdc6b54128fb2df70ae7
PankajMaurya <maurya2498@gmail.com>
2025-05-27 17:21:08 +0530

    Min_Max Function

--- Array/Max_Min.java
+++ Array/Max_Min.java
@@ -3,6 +3,9 @@ package Array;
 public class Max_Min {
 
     static int Max(int[] arr) {
+        if (arr.length == 0)
+            return -1;
+
         int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if (max < arr[i]) {
@@ -11,8 +14,26 @@ public class Max_Min {
         }
         return max;
     }
-    
+
+    static int MaxRange(int[] arr, int start, int end) {
+
+        if ((start > end) || (arr.length == 0))
+            return -1;
+
+        int max = arr[start];
+        for (int i = start; i < end; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
     static int Min(int[] arr) {
+
+        if (arr.length == 0)
+            return -1;
+
         int min = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if (min > arr[i]) {
@@ -22,13 +43,30 @@ public class Max_Min {
         return min;
     }
 
+    static int MinRange(int[] arr, int start, int end) {
+        if ((start > end) || (arr.length == 0))
+            return -1;
+
+        int min = arr[start];
+        for (int i = start; i < end; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
 
-        int[] arr = { 12, 32, 45, 65, 75 };
+        int[] arr = {23, 43, 54, 67, 76, 87};
 
         System.out.println(Min(arr));
-        
+
+        System.out.println(MinRange(arr, 1, 4));
+
         System.out.println(Max(arr));
+
+        System.out.println(MaxRange(arr, 1, 4));
 
     }
 }
