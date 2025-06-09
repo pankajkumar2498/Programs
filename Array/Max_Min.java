249684478fcee8d17f34db414e1680703fcbba4d
PankajMaurya <maurya2498@gmail.com>
2025-06-09 17:36:23 +0530

    Updated Min_Max using BinarySerach

--- Array/Max_Min.java
+++ Array/Max_Min.java
@@ -45,6 +45,24 @@ public class Max_Min {
         return min;
     }
 
+    // Find Minimum using BinarySearch
+    public int findMin(int[] nums) {
+
+        int start = 0;
+        int end = nums.length -1;
+
+        while(start < end){
+            int mid = start + (end - start)/2;
+
+            if( nums[mid] > nums[end] ){
+                start = mid + 1;
+            }else if(nums[mid] < nums[end]){
+                end = mid;
+            }
+        }
+        return nums[start];      
+    }
+
     static int MinRange(int[] arr, int start, int end) {
         if ((start > end) || (arr.length == 0))
             return -1;
