da0d4ef7100b964b8c15078470c863fb6bd4cf62
PankajMaurya <maurya2498@gmail.com>
2025-06-24 17:36:34 +0530

    Happy Number

--- HappyNumber.java
+++ HappyNumber.java
@@ -1,24 +1,26 @@
+// A number is called happy if it leads to 1 after a sequence of steps wherein each step number is replaced 
+//by the sum of squares of its digit that is if we start with Happy Number and keep replacing it with digits square sum, we reach 1. 
+
+// Examples : 
+// Input: n = 19
+// Output: True
+// 19 is Happy Number,
+// 1^2 + 9^2 = 82
+// 8^2 + 2^2 = 68
+// 6^2 + 8^2 = 100
+// 1^2 + 0^2 + 0^2 = 1
+// As we reached to 1, 19 is a Happy Number.
+
+// Input: n = 20
+// Output: False
+
 import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
 
 public class HappyNumber {
 
-    public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("Enter number: ");
-        int num = scan.nextInt();
-
-        System.out.println(isHappy(num));
-
-        scan.close();
-    }
-
     public static boolean isHappy(int n) {
 
-        Set<Integer> usedNumbers = new HashSet<>();
+        HashSet<Integer> usedNumbers = new HashSet<>();
 
         while (true) {
 
@@ -40,5 +42,36 @@ public class HappyNumber {
                 usedNumbers.add(n);
             }
         }
+    }
+
+    static int numSquareSum(int n) {
+        int num = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            num += digit * digit;
+            n /= 10;
+        }
+        return num;
+    }
+
+    // method return true if n is Happy Number
+    static boolean isHappyNumber(int n) {
+        HashSet<Integer> st = new HashSet<>();
+        while (true) {
+            n = numSquareSum(n);
+            if (n == 1)
+                return true;
+            if (st.contains(n))
+                return false;
+            st.add(n);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int num = 19;
+        System.out.println();
+        System.out.println(isHappyNumber(num));
+
     }
 }
