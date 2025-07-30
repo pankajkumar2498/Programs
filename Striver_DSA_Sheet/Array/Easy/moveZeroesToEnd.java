78c2b342260548b851d6bc6a08e24cbadb924961
PankajMaurya <maurya2498@gmail.com>
2025-07-30 17:05:15 +0530

    MoveZeroes to End

--- Striver_DSA_Sheet/Array/Easy/moveZeroesToEnd.java
+++ Striver_DSA_Sheet/Array/Easy/moveZeroesToEnd.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 
 public class moveZeroesToEnd {
 
-
-    // Brute Force Approach
-    static void moveZeroes1(int[] arr) {
+    // Brute Force Approach --> Returns same Array with all zeroes in end
+    static void moveZeroes(int[] arr) {
 
         int[] temp = new int[arr.length];
-
+        int index = 0; // Tracks the position in temp to insert non-zero elements
+        
         // copy non zero elements in temporary array
         for (int i = 0; i <= arr.length - 1; i++) {
             if (arr[i] != 0) {
-                temp[i] = arr[i];
+                temp[index++] = arr[i];
             }
         }
 
@@ -30,9 +30,26 @@ public class moveZeroesToEnd {
         System.out.println(Arrays.toString(arr));
     }
 
+    // Returns new Array with all Zeroes in end
+    static int[] moveZeroes1(int[] arr) {
+        int[] result = new int[arr.length];
+        int index = 0;
+
+        // Add all non-zero elements to the result array
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                result[index++] = arr[i];
+            }
+        }
+
+        // Remaining elements are already 0 by default
+        System.out.println(Arrays.toString(result));    // for testing purpose only
+        return result;
+    }
+
 
     // Optimal Approach --> O(N)
-    static void moveZeroes(int[] arr) {
+    static void moveZeroes2(int[] arr) {
 
         int zeroIndex = -1; // we can take zeroIndex as j also.
 
@@ -45,11 +62,11 @@ public class moveZeroesToEnd {
         }
 
 
-        // if no zeroes founf then return
+        // if no zeroes found then return
         if (zeroIndex == -1)
             return;
 
-        // start i index from next index where zero foung i.e, zeroIndex
+        // start i index from next index where zero found i.e, zeroIndex
         for (int i = zeroIndex + 1; i < arr.length; i++) {
             if (arr[i] != 0) {
 
@@ -66,9 +83,9 @@ public class moveZeroesToEnd {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 0, 4, 0, 33, 0, 40, 50 };
+        int[] arr = { 0, 64, 0, 33, 0, 40, 50 };
 
-        moveZeroes(arr);
+        moveZeroes2(arr);
 
     }
 
