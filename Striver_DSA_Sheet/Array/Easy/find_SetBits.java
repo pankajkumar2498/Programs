ffea1bdbb8dc3bcdf48a4ce4fb26673ee62c5968
PankajMaurya <maurya2498@gmail.com>
2025-07-31 14:25:07 +0530

    Find Set Bits in any Number

--- Striver_DSA_Sheet/Array/Easy/find_SetBits.java
+++ Striver_DSA_Sheet/Array/Easy/find_SetBits.java
@@ -2,14 +2,14 @@ package Striver_DSA_Sheet.Array.Easy;
 
 public class find_SetBits {
 
-     // Optimal Approach --> O(N)
+    // Optimal Approach --> O(N)
     static void findSetBits1(int num) {
         int count = 0;
-        while (num != 0){
+        while (num != 0) {
             num = num & (num - 1);
-            count ++;
+            count++;
         }
-        System.out.println(count);
+        System.out.println("Total Set Bits: " + count);
     }
 
     // Better Approach --> O(N)
@@ -17,25 +17,21 @@ public class find_SetBits {
         int count = 0;
         for (int i = 0; i <= 31; i++) {
 
-            int bit = 1 << i;           // always left shift by using 1
+            int bit = 1 << i; // always left shift by using 1
 
             if ((num & bit) != 0) {
                 count++;
             }
         }
-        System.out.println(count);
+        System.out.println("Total Set Bits: " + count);
     }
 
     // this function sets nth-bit to 0 where n can be any number.
     // static void setBitsToZero(int num) {
-
     //     int count = 0;
     //     for (int i = 0; i <= 31; i++) {
-
     //         int bit = 1 << i;
-
     //     }
-
     // }
 
     public static void main(String[] args) {
